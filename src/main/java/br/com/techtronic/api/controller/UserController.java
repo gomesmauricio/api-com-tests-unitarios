1ff1599300cf37fd345300da52aae83bbaab827d
Mauricio <Tit@m125>
2022-03-04 21:41:59 -0300

    classe user controller findbyid

--- src/main/java/br/com/techtronic/api/controller/UserController.java
+++ src/main/java/br/com/techtronic/api/controller/UserController.java
@@ -1,18 +1,23 @@
 package br.com.techtronic.api.controller;
 
 import br.com.techtronic.api.domain.User;
+import br.com.techtronic.api.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    private UserService userService;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1, "Mauricio", "mauricio@mail.com", "123"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }
