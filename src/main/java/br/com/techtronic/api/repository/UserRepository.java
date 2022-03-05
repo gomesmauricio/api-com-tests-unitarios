189e1ed611a0575a7ea5db45de684312f809ee68
Mauricio <Tit@m125>
2022-03-04 21:40:00 -0300

    interface user repository

---
+++ src/main/java/br/com/techtronic/api/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.techtronic.api.repository;
+
+import br.com.techtronic.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
